f95997836eb5a802896ba269e41a14e0f3f98c52
Yash Bansal <dilboleoberoy@gmail.com>
2024-10-19 16:04:30 +0530

    about and contact

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule,MatButtonModule,MatInputModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
