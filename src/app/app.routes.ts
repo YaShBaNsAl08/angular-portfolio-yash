033f45c409996d0b0f953e2f8dce4ae6ce24aaef
Yash Bansal <dilboleoberoy@gmail.com>
2025-04-22 22:37:34 +0530

    certificates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { CertificatesComponent } from './components/certificates/certificates.component';
 
 export const routes: Routes = [
     {path: '', redirectTo:'/home',pathMatch:'full'},
@@ -10,5 +11,6 @@ export const routes: Routes = [
     {path: 'about', component:AboutComponent},
     {path: 'project', component:ProjectsComponent},
     {path: 'contact', component:ContactComponent},
+    {path: 'certificates', component:CertificatesComponent},
     {path: '**', redirectTo:'/home',pathMatch:'full'},
 ];
