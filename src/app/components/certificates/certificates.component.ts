bf91d00f4e54a729d8887fd07b493ed085554cb7
Yash Bansal <dilboleoberoy@gmail.com>
2025-04-25 23:30:26 +0530

    certificate structure

--- src/app/components/certificates/certificates.component.ts
+++ src/app/components/certificates/certificates.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import * as AOS from 'aos';
 @Component({
   selector: 'app-certificates',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,MatCardModule,MatButtonModule],
   templateUrl: './certificates.component.html',
   styleUrl: './certificates.component.css'
 })
-export class CertificatesComponent {
-
+export class CertificatesComponent implements OnInit{
+  ngOnInit(): void {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }
+  ngAfterViewInit(): void {
+    AOS.refresh();
+  }
+  certificates = [
+    {
+      title: 'Azure-204 Bootcamp',
+      platform: 'Microsoft',
+      link: '',
+      imageUrl: '',
+    },
+    {
+      title: 'AWS WebStorm Hackathon',
+      platform: 'AWS',
+      link: '',
+      imageUrl: '',
+    },
+    {
+      title: 'Trailhead Ranger Badge',
+      platform: 'Salesforce',
+      link: '',
+      imageUrl: '',
+    }
+  ];
 }
