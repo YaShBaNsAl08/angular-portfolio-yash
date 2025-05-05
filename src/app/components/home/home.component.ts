a2769a438fbba1e9216b5fbeb2a17b4ea72921a3
Yash Bansal <dilboleoberoy@gmail.com>
2025-05-05 23:23:05 +0530

    button

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,22 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { trigger,style,transition,animate } from '@angular/animations';
+import * as AOS from 'aos';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatButtonModule,RouterLink],
+  imports: [MatButtonModule,RouterLink,MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   animations:[trigger('fadeIn',[transition(':enter',[style({opacity:0}),animate('1s ease-in', style({opacity:1}))])])]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 text='';
 f_text='A passionate Front-end Web Developer';
 i=0;
 constructor(){
   this.typetext();
 }
+  ngOnInit(): void {
+    AOS.init();
+  }
 typetext(){
   if(this.i<this.f_text.length){
     setTimeout(()=>{
